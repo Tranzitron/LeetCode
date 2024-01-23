7a554ad46fa565e5eacfe7c20c9f15e0dd22b52e
Raphael Vallée <72668371+Tranzitron@users.noreply.github.com>
2024-01-23 13:30:58 -0500

    WIP - LeetCode 20. Valid Parentheses

---
+++ LeetCode 20. Valid Parentheses/Tests.cs
@@ -0,0 +1,50 @@
+namespace LeetCode_20._Valid_Parentheses;
+
+public class Tests
+{
+    private readonly Solution _solution = new();
+
+    [Fact]
+    public void Example1()
+    {
+        var str = "()";
+
+        var actualOutput = _solution.IsValid(str);
+        var expectedOutput = true;
+
+        Assert.Equal(expectedOutput, actualOutput);
+    }
+
+    [Fact]
+    public void Example2()
+    {
+        var str = "()[]{}";
+
+        var actualOutput = _solution.IsValid(str);
+        var expectedOutput = true;
+
+        Assert.Equal(expectedOutput, actualOutput);
+    }
+
+    [Fact]
+    public void Example3()
+    {
+        var str = "(]";
+
+        var actualOutput = _solution.IsValid(str);
+        var expectedOutput = false;
+
+        Assert.Equal(expectedOutput, actualOutput);
+    }
+    
+    [Fact]
+    public void TestCase80()
+    {
+        var str = "([)]";
+
+        var actualOutput = _solution.IsValid(str);
+        var expectedOutput = false;
+
+        Assert.Equal(expectedOutput, actualOutput);
+    }
+}
